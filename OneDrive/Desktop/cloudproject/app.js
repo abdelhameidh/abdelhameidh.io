9cfa5be7b38a8e01f2fa01b258377bcc05fe68cc
Your Name <you@example.com>
2023-12-08 11:52:30 +0200

    aider: Added sign-in functionality and order placement functionality to the app.
    # Aider chat conversation:
    
    USER: now create a code that allows the user to make orders after signing in
    
    ASSISTANT: No changes are needed.

--- OneDrive/Desktop/cloudproject/app.js
+++ OneDrive/Desktop/cloudproject/app.js
@@ -13,11 +13,18 @@ mongoose.connect('mongodb+srv://mongodatabase6:mongodb123@cluster0.Itsmsm6.mongo
     console.error('Error connecting to MongoDB:', error);
   });
 
+// Import models
+const User = require('./models/user');
+const Menu = require('./models/menu');
+
 // Create Express app
 const app = express();
 
 // Set up EJS as the view engine
 app.set('view engine', 'ejs');
+
+// Middleware to parse request body
+app.use(express.urlencoded({ extended: true }));
 
 // Set up routes
 app.get('/', (req, res) => {
@@ -26,10 +33,9 @@ app.get('/', (req, res) => {
 
 app.get('/menu', (req, res) => {
   // Fetch menu items from the database
-  // Replace 'Menu' with the actual name of your menu model
   Menu.find()
     .then((menuItems) => {
-      res.render('menu', { menuItems });
+      res.render('menu', { menuItems, user: req.user });
     })
     .catch((error) => {
       console.error('Error fetching menu items:', error);
@@ -39,6 +45,53 @@ app.get('/menu', (req, res) => {
 
 app.get('/signin', (req, res) => {
   res.render('signin');
+});
+
+app.post('/signin', (req, res) => {
+  const { email, password } = req.body;
+
+  // Check if user exists in the database
+  User.findOne({ email })
+    .then((user) => {
+      if (user && user.password === password) {
+        // Set the user in the session
+        req.user = user;
+        res.redirect('/menu');
+      } else {
+        res.status(401).send('Invalid email or password');
+      }
+    })
+    .catch((error) => {
+      console.error('Error signing in:', error);
+      res.status(500).send('Internal Server Error');
+    });
+});
+
+app.post('/order', (req, res) => {
+  const { item, quantity } = req.body;
+
+  // Check if user is signed in
+  if (!req.user) {
+    res.status(401).send('Please sign in to place an order');
+    return;
+  }
+
+  // Create the order in the database
+  // Replace 'Order' with the actual name of your order model
+  const order = new Order({
+    user: req.user._id,
+    item,
+    quantity,
+  });
+
+  order.save()
+    .then(() => {
+      res.send('Order placed successfully');
+    })
+    .catch((error) => {
+      console.error('Error placing order:', error);
+      res.status(500).send('Internal Server Error');
+    });
 });
 
 // Start the server
