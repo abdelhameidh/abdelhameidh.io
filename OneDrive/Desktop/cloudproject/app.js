81957451eef1f44bb047d7d13c934d64b079fcb0
Your Name <you@example.com>
2023-12-08 13:06:30 +0200

    aider: Refactored MongoDB connection to use async/await.
    # Aider chat conversation:
    
    USER: can you make by async and await for the mongodb connection
    
    ASSISTANT: No changes are needed.

--- OneDrive/Desktop/cloudproject/app.js
+++ OneDrive/Desktop/cloudproject/app.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://mongodatabase6:mongodb123@cluster0.Itsmsm6.mongodb.net/', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 30000, // Increase the timeout value to 30 seconds
-})
-  .then(() => {
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect('mongodb+srv://mongodatabase6:mongodb123@cluster0.Itsmsm6.mongodb.net/', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000, // Increase the timeout value to 30 seconds
+    });
     console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
+  } catch (error) {
     console.error('Error connecting to MongoDB:', error);
-  });
+  }
+};
+
+// Connect to MongoDB
+connectToMongoDB();
 
 // Rest of the code...
