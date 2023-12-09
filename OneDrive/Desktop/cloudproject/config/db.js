dc4a7d88c26e8edd40d21fd68cc4ab0144ec3cb3
Your Name <you@example.com>
2023-12-09 17:22:12 +0200

    aider: Refactor MongoDB connection string to include the database name.
    # Aider chat conversation:
    
    ASSISTANT: No changes are needed.

--- OneDrive/Desktop/cloudproject/config/db.js
+++ OneDrive/Desktop/cloudproject/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://mongodatabase6:mongodb123@cluster0.Itsmsm6.mongodb.net/');
+    await mongoose.connect('mongodb+srv://mongodatabase6:mongodb123@cluster0.Itsmsm6.mongodb.net/your-database-name');
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
