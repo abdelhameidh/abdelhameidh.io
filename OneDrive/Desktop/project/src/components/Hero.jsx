34f02c77f412a3bd20a87afc0749a93c5c731565
abdelhameidh <132206702+abdelhameidh@users.noreply.github.com>
2024-11-26 22:55:47 +0200

    Update Hero.jsx

--- OneDrive/Desktop/project/src/components/Hero.jsx
+++ OneDrive/Desktop/project/src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import Section from "./section";
+import Section from "./Section";
 import Button from "./Button";
 import ButtonUpload from "./ButtonUpload";
 import { curve, heroBackground, robot } from "../assets";
