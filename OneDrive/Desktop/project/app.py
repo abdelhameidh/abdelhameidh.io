202c6f8028717b5f955ff2fbe2548ff4093da3d7
Your Name <you@example.com>
2024-11-26 16:16:14 +0200

    Add large files tracked by Git LFS

---
+++ OneDrive/Desktop/project/app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:36dde20ef686691c9ce8e42f6362c193d2445031bdae5d6ce7af4c867f275f24
+size 4118
---
+++ OneDrive/Desktop/project/musicgen.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d9ca93854fd554d021c50bdf5d58b31b9b522f7191c3a1d849c7bdc2f6ece00b
+size 2900
